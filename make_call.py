3c3868b18dfa316256a0b169945532fbbb6e6fd0
Chad Selph <chad.selph@gmail.com>
2012-10-12 14:02:04 -0700

    fix typo for calls

--- make_call.py
+++ make_call.py
@@ -8,4 +8,4 @@ FROM_NUMBER = ''     # Your Twilio phone number
 TWIML_URL = 'http://twimlets.com/message?Message[1]=Hello+World'
 
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-client.calls.messages.create(to=TO_NUMBER, from_=FROM_NUMBER, url=TWIML_URL)
+client.calls.create(to=TO_NUMBER, from_=FROM_NUMBER, url=TWIML_URL)
